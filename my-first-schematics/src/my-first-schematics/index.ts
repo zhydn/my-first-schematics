ed5d33288ae608764aac53a63e2cb0a9130c8547
zhanghy <zhanghy@benchmarkchina.com>
2021-06-09 11:38:18 +0800

    finish

--- my-first-schematics/src/my-first-schematics/index.ts
+++ my-first-schematics/src/my-first-schematics/index.ts
@@ -1,10 +1,39 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { join, normalize, strings } from '@angular-devkit/core';
+import { apply, chain, MergeStrategy, mergeWith, move, Rule, SchematicContext, template, Tree, url } from '@angular-devkit/schematics';
+import { parseName } from '@schematics/angular/utility/parse-name';
+import { buildDefaultPath, getWorkspace } from '@schematics/angular/utility/workspace';
 
-
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 export function myFirstSchematics(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+  return async (tree: Tree, _context: SchematicContext) => {
+    let workspace, project, defaultProjectPath;
+    try {
+      workspace = await getWorkspace(tree);
+      if (!_options.project) {
+        _options.project = workspace.projects.keys().next().value;
+      }
+      project = workspace.projects.get(_options.project);
+      defaultProjectPath = buildDefaultPath(project);
+      _options.path = join(normalize(project.root), 'src');
+    } catch (error) {
+      if (!_options.path) {
+        _options.path = '/src/';
+      }
+      defaultProjectPath = _options.path;
+    }
+
+    const parsedPath = parseName(defaultProjectPath, _options.name);
+    const { name, path } = parsedPath;
+    const sourceTemplate = url('./files');
+    const templateSource = apply(sourceTemplate, [
+      template({ ..._options, ...strings, name, 自我介绍 }),
+      move(path)
+    ]);
+
+    return chain([mergeWith(templateSource, MergeStrategy.Overwrite)]);
   };
+}
+
+
+function 自我介绍(name: string, sex: string, food: string) {
+  return `my name is ${name}, is ${sex ? 'boy' : 'girl'},the food is ${food}`;
 }
--- remade.md
+++ remade.md
@@ -1,2 +1,7 @@
 1. schematics blank my-first-schematics
 2. ng new my-app
+
+
+
+
+schematics ./my-first-schematics/src/collection.json:mfs zhy --debug=false
